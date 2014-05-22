2e2d80d2f49829e288aaaf71c183234e8f49b34d
Victor Cotrim <cotrim149@gmail.com>
2014-05-22 16:08:51 -0300

    Commit Inicial

---
+++ ParOuImpar/ParOuImpar/VCLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VCLAppDelegate.h
+//  ParOuImpar
+//
+//  Created by Victor de Lima on 22/05/14.
+//  Copyright (c) 2014 Victor de Lima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VCLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
